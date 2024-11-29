eb3c41f35a5ec18241a728fcb4e7c6c0395fe7b1
Vlad Jerca <vlad.jerca@gmail.com>
2024-11-29 11:53:31 +0100

    fix(episode): use episode runtime rather than show runtime (better eta))

--- projects/api/src/contracts/_internal/response/episodeResponseSchema.ts
+++ projects/api/src/contracts/_internal/response/episodeResponseSchema.ts
@@ -9,6 +9,34 @@ export const episodeResponseSchema = z.object({
   /***
    * Available if requesting extended `full`.
    */
+  rating: z.number(),
+  /***
+   * Available if requesting extended `full`.
+   */
+  votes: z.number(),
+  /***
+   * Available if requesting extended `full`.
+   */
+  comment_count: z.number(),
+  /***
+   * Available if requesting extended `full`.
+   */
+  updated_at: z.string(),
+  /***
+   * Available if requesting extended `full`.
+   */
+  available_translations: z.array(z.string()),
+  /***
+   * Available if requesting extended `full`.
+   */
+  runtime: z.number(),
+  /***
+   * Available if requesting extended `full`.
+   */
+  overview: z.string(),
+  /***
+   * Available if requesting extended `full`.
+   */
   episode_type: z
     .enum([
       `standard`,

50e16cdbf85dafe9636c7003e81cbceb138cedb4
Vlad Jerca <vlad.jerca@gmail.com>
2024-12-04 16:11:04 +0100

    feat(api): define oauth token exchange endpoint

--- projects/api/src/contracts/oauth/_internal/request/deviceTokenRequestSchema.ts
+++ projects/api/src/contracts/oauth/_internal/request/deviceTokenRequestSchema.ts
@@ -1,9 +1,9 @@
 import { z } from '../../../_internal/z.ts';
 
-export const tokenRequestSchema = z.object({
+export const deviceTokenRequestSchema = z.object({
   code: z.string({
     description:
-      'The code received when trakt redirects the user back to the application.',
+      'The device code received from the device authorization request.',
   }),
   client_id: z.string({
     description: `The client ID of the application. 
@@ -13,8 +13,4 @@ export const tokenRequestSchema = z.object({
     description: `The client secret of the application. 
             You can find it in the application details here: https://trakt.tv/oauth/applications`,
   }),
-  redirect_uri: z.string({
-    description: 'URI specified in your app settings.',
-  }),
-  grant_type: z.literal('authorization_code'),
 });
--- projects/api/src/contracts/oauth/index.ts
+++ projects/api/src/contracts/oauth/index.ts
@@ -1,6 +1,9 @@
 import { builder } from '../_internal/builder.ts';
 import { z } from '../_internal/z.ts';
 import { codeRequestSchema } from './_internal/request/codeRequestSchema.ts';
+import {
+  deviceTokenRequestSchema,
+} from './_internal/request/deviceTokenRequestSchema.ts';
 import { tokenRequestSchema } from './_internal/request/tokenRequestSchema.ts';
 import { codeResponseSchema } from './_internal/response/codeResponseSchema.ts';
 import { tokenResponseSchema } from './_internal/response/tokenResponseSchema.ts';
@@ -17,7 +20,7 @@ const device = builder.router({
   token: {
     path: '/token',
     method: 'POST',
-    body: tokenRequestSchema,
+    body: deviceTokenRequestSchema,
     responses: {
       200: tokenResponseSchema,
       400: z.undefined(),
@@ -29,12 +32,23 @@ const device = builder.router({
 
 export type OAuthDeviceCodeRequest = z.infer<typeof codeRequestSchema>;
 export type OAuthDeviceCodeResponse = z.infer<typeof codeResponseSchema>;
-export type OAuthDeviceTokenRequest = z.infer<typeof tokenRequestSchema>;
-export type OAuthDeviceTokenResponse = z.infer<typeof tokenResponseSchema>;
+export type OAuthDeviceTokenRequest = z.infer<typeof deviceTokenRequestSchema>;
+
+export type OAuthTokenRequest = z.infer<typeof tokenRequestSchema>;
+export type OAuthTokenResponse = z.infer<typeof tokenResponseSchema>;
 
 export const oauth = builder
   .router({
     device,
+    token: {
+      path: '/token',
+      method: 'POST',
+      body: tokenRequestSchema,
+      responses: {
+        200: tokenResponseSchema,
+        400: z.undefined(),
+      },
+    },
   }, {
     pathPrefix: '/oauth',
   });

6313093a9d4a5f3758ec4669d53b3aaffeadcc96
seferturan <seferturan85@gmail.com>
2024-12-16 19:39:23 +0100

    feat(request): map trakt ratings

--- projects/api/src/contracts/_internal/response/ratingsResponseSchema.ts
+++ projects/api/src/contracts/_internal/response/ratingsResponseSchema.ts
@@ -1,19 +1,21 @@
 import { z } from '../z.ts';
 
 export const ratingsResponseSchema = z.object({
-  rating: z.number(),
-  votes: z.number(),
-  distribution: z.object({
-    1: z.number(),
-    2: z.number(),
-    3: z.number(),
-    4: z.number(),
-    5: z.number(),
-    6: z.number(),
-    7: z.number(),
-    8: z.number(),
-    9: z.number(),
-    10: z.number(),
+  trakt: z.object({
+    rating: z.number(),
+    votes: z.number(),
+    distribution: z.object({
+      1: z.number(),
+      2: z.number(),
+      3: z.number(),
+      4: z.number(),
+      5: z.number(),
+      6: z.number(),
+      7: z.number(),
+      8: z.number(),
+      9: z.number(),
+      10: z.number(),
+    }),
   }),
   /***
    * Available if requesting extended `all`.

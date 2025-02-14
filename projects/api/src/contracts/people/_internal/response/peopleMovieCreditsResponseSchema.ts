766d18a19dfe79a2856cd8714e636c8663928559
seferturan <seferturan85@gmail.com>
2025-02-14 16:29:31 +0100

    feat(api): add definition for crew credits responses

--- projects/api/src/contracts/people/_internal/response/peopleMovieCreditsResponseSchema.ts
+++ projects/api/src/contracts/people/_internal/response/peopleMovieCreditsResponseSchema.ts
@@ -1,4 +1,6 @@
 import { characterResponseSchema } from '../../../_internal/response/characterResponseSchema.ts';
+import { crewPositionResponseSchema } from '../../../_internal/response/crewPositionResponseSchema.ts';
+import { jobsResponseSchema } from '../../../_internal/response/jobsResponseSchema.ts';
 import { movieResponseSchema } from '../../../_internal/response/movieResponseSchema.ts';
 import { z } from '../../../_internal/z.ts';
 
@@ -7,5 +9,13 @@ export const peopleMovieCreditsResponseSchema = z.object({
     z.object({
       movie: movieResponseSchema,
     }).merge(characterResponseSchema),
+  ).optional(),
+  crew: z.record(
+    crewPositionResponseSchema,
+    z.array(
+      z.object({
+        movie: movieResponseSchema,
+      }).merge(jobsResponseSchema),
+    ),
   ).optional(),
 });
--- projects/api/src/contracts/people/_internal/response/peopleShowCreditsResponseSchema.ts
+++ projects/api/src/contracts/people/_internal/response/peopleShowCreditsResponseSchema.ts
@@ -1,4 +1,6 @@
 import { characterResponseSchema } from '../../../_internal/response/characterResponseSchema.ts';
+import { crewPositionResponseSchema } from '../../../_internal/response/crewPositionResponseSchema.ts';
+import { jobsResponseSchema } from '../../../_internal/response/jobsResponseSchema.ts';
 import { showResponseSchema } from '../../../_internal/response/showResponseSchema.ts';
 import { z } from '../../../_internal/z.ts';
 
@@ -9,5 +11,14 @@ export const peopleShowCreditsResponseSchema = z.object({
       episode_count: z.number(),
       series_regular: z.boolean(),
     }).merge(characterResponseSchema),
+  ).optional(),
+  crew: z.record(
+    crewPositionResponseSchema,
+    z.array(
+      z.object({
+        show: showResponseSchema,
+        episode_count: z.number(),
+      }).merge(jobsResponseSchema),
+    ),
   ).optional(),
 });

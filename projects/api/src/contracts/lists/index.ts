435ee24289336358a2852c82e58ff3309b1af2c7
seferturan <seferturan85@gmail.com>
2025-02-19 17:21:33 +0100

    feat(api): add support for getting lists summary using lists endpoint

--- projects/api/src/contracts/lists/index.ts
+++ projects/api/src/contracts/lists/index.ts
@@ -4,10 +4,20 @@ import { idParamsSchema } from '../_internal/request/idParamsSchema.ts';
 import { pageQuerySchema } from '../_internal/request/pageQuerySchema.ts';
 import { listedMovieResponseSchema } from '../_internal/response/listedMovieResponseSchema.ts';
 import { listedShowResponseSchema } from '../_internal/response/listedShowResponseSchema.ts';
+import { listResponseSchema } from '../_internal/response/listResponseSchema.ts';
 import { z } from '../_internal/z.ts';
 import { searchTypeParamFactory } from '../search/_internal/request/searchTypeParamFactory.ts';
 
 export const lists = builder.router({
+  summary: {
+    path: '',
+    method: 'GET',
+    pathParams: idParamsSchema,
+    query: extendedQuerySchemaFactory<['full', 'images']>(),
+    responses: {
+      200: listResponseSchema,
+    },
+  },
   items: {
     path: '/items/:type',
     method: 'GET',
